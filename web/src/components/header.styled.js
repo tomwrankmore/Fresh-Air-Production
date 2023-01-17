70bda9b59e559e00841c5194f144d63f31792ba1
tomwrankmore <35687163+tomwrankmore@users.noreply.github.com>
2023-01-17 12:54:40 +0000

    nav gradient

--- web/src/components/header.styled.js
+++ web/src/components/header.styled.js
@@ -7,7 +7,9 @@ export const Wrapper = styled.div`
   padding: 1em;
   display: flex;
   justify-content: flex-end;
-  background: linear-gradient(0deg, rgba(255, 255, 255, 0) 0%, rgba(255, 255, 255, 0.75) 100%);
+  @media ${device.mediaMinMedium} {
+    background: linear-gradient(0deg, rgba(255, 255, 255, 0) 0%, rgba(255, 255, 255, 0.75) 100%);
+  }
 
   @media ${device.mediaMinLarge} {
     padding: 1.5em 1.5em;
