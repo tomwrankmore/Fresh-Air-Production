fea0b6381f951409d86de8993a6ad2156e3a9896
tomwrankmore <35687163+tomwrankmore@users.noreply.github.com>
2023-02-10 11:32:01 +0000

    terms

---
+++ studio/schemas/documents/terms.js
@@ -0,0 +1,12 @@
+export default {
+  name: 'terms',
+  title: 'Terms',
+  type: 'document',
+  fields: [
+    {
+      name: 'body',
+      title: 'Body',
+      type: 'projectPortableText'
+    }
+  ]
+}
