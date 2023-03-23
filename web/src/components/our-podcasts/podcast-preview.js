96dd8e3955ae187f8c9ba96d40b5ed804a7cd320
tomwrankmore <35687163+tomwrankmore@users.noreply.github.com>
2023-03-23 10:12:30 +0000

    ..

--- web/src/components/our-podcasts/podcast-preview.js
+++ web/src/components/our-podcasts/podcast-preview.js
@@ -112,8 +112,11 @@ const PodcastPreview = ({node}) => {
           </Excerpt>
         )}
         <LinkContent>
-            {!!node.hasPage && <Link to={`/podcast/${node.slug.current}`} className="read-more-link"><BsFillPlayFill/>Play</Link>}
-            {/* <a href="https://omny.fm/shows/fresh-ears" target="_blank" rel="noreferrer" className="read-more-link"><BsFillPlayFill/> Play</a> */}
+            {/* {!!node.hasPage && <Link to={`/podcast/${node.slug.current}`} className="read-more-link"><BsFillPlayFill/>Play</Link>} */}
+            {!!node.omnyWidget ?
+                <a href={`https://omny.fm/shows/${node.omnyWidget}`} target="_blank" rel="noreferrer" className="read-more-link"><BsFillPlayFill/> Play</a> 
+                : null
+              }
             <IconList>
               {node.podcastLink && (<li><a href={node.podcastLink} target="_blank" rel="noreferrer"><FaPodcast/></a></li>)}
               {node.spotifyLink && (<li><a href={node.spotifyLink} target="_blank" rel="noreferrer"><FaSpotify/></a></li>)}
