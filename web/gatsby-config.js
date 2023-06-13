cf7c57084ed123a8d8744ef80c885740c53bd077
tomwrankmore <35687163+tomwrankmore@users.noreply.github.com>
2023-06-13 13:40:59 +0100

    google tag GA4

--- web/gatsby-config.js
+++ web/gatsby-config.js
@@ -62,18 +62,38 @@ module.exports = {
           timeout: 3500, // number; the amount of time, in milliseconds, that you want to allow mailchimp to respond to your request before timing out. defaults to 3500
       },
     },
+    // {
+    //   resolve: `gatsby-plugin-google-analytics`,
+    //   options: {
+    //     // The property ID; the tracking code won't be generated without it
+    //     trackingId: process.env.GA_TRACKING_ID,
+    //   },
+    // },
+    // {
+    //   resolve: "gatsby-plugin-google-tagmanager",
+    //   options: {
+    //     id: "G-94HGRECTKX",
+    //     includeInDevelopment: false,
+    //   },
+    // },
     {
-      resolve: `gatsby-plugin-google-analytics`,
+      resolve: `gatsby-plugin-google-gtag`,
       options: {
-        // The property ID; the tracking code won't be generated without it
-        trackingId: process.env.GA_TRACKING_ID,
-      },
-    },
-    {
-      resolve: "gatsby-plugin-google-tagmanager",
-      options: {
-        id: "G-94HGRECTKX",
-        includeInDevelopment: false,
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "G-94HGRECTKX", // Google Analytics / GA
+          "AW-970211384", // Google Ads / Adwords / AW
+          // "DC-FLOODIGHT_ID", // Marketing Platform advertising products (Display & Video 360, Search Ads 360, and Campaign Manager)
+        ],
+        // This object gets passed directly to the gtag config command
+        // This config will be shared across all trackingIds
+        gtagConfig: {
+          // optimize_id: "OPT_CONTAINER_ID",
+          // anonymize_ip: true,
+          // cookie_expires: 0,
+        },
+        pluginConfig: {
+        },
       },
     },
   ]
