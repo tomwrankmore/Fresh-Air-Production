f955ba19a6915dbaf3efd21ac91bf4e185663208
tomwrankmore <35687163+tomwrankmore@users.noreply.github.com>
2024-02-21 18:34:47 +0000

    horiz scroll

--- web/src/components/horizontal-scroll-section/horizontal-scroll-section.gsap.js
+++ web/src/components/horizontal-scroll-section/horizontal-scroll-section.gsap.js
@@ -26,56 +26,56 @@ function horizontalContainerAnim(containerRef) {
       // ScrollTrigger.defaults({ markers: { startColor: "white", endColor: "white" } });
 
       // red section
-      gsap.to(".box1", {
-        y: -130,
-        duration: 2,
-        ease: "elastic",
-        scrollTrigger: {
-          trigger: ".box1",
-          containerAnimation: scrollTween,
-          start: "left center",
-          toggleActions: "play none none reset",
-          id: "1"
-        }
-      });
+      // gsap.to(".box1", {
+      //   y: -130,
+      //   duration: 2,
+      //   ease: "elastic",
+      //   scrollTrigger: {
+      //     trigger: ".box1",
+      //     containerAnimation: scrollTween,
+      //     start: "left center",
+      //     toggleActions: "play none none reset",
+      //     id: "1"
+      //   }
+      // });
 
       // gray section
-      gsap.to(".box2", {
-        y: -120,
-        backgroundColor: "#1e90ff",
-        ease: "none",
-        scrollTrigger: {
-          trigger: ".box2",
-          containerAnimation: scrollTween,
-          start: "center 80%",
-          end: "center 20%",
-          scrub: true,
-          id: "2"
-        }
-      });
+      // gsap.to(".box2", {
+      //   y: -120,
+      //   backgroundColor: "#1e90ff",
+      //   ease: "none",
+      //   scrollTrigger: {
+      //     trigger: ".box2",
+      //     containerAnimation: scrollTween,
+      //     start: "center 80%",
+      //     end: "center 20%",
+      //     scrub: true,
+      //     id: "2"
+      //   }
+      // });
 
       // purple section
-      ScrollTrigger.create({
-        trigger: ".box3",
-        containerAnimation: scrollTween,
-        toggleClass: "active",
-        start: "center 60%",
-        id: "3"
-      });
+      // ScrollTrigger.create({
+      //   trigger: ".box3",
+      //   containerAnimation: scrollTween,
+      //   toggleClass: "active",
+      //   start: "center 60%",
+      //   id: "3"
+      // });
 
       // green section
-      ScrollTrigger.create({
-        trigger: ".green",
-        containerAnimation: scrollTween,
-        start: "center 65%",
-        end: "center 51%",
-        onEnter: () => console.log("enter"),
-        onLeave: () => console.log("leave"),
-        onEnterBack: () => console.log("enterBack"),
-        onLeaveBack: () => console.log("leaveBack"),
-        onToggle: self => console.log("active", self.isActive),
-        id: "4"
-      });
+      // ScrollTrigger.create({
+      //   trigger: ".green",
+      //   containerAnimation: scrollTween,
+      //   start: "center 65%",
+      //   end: "center 51%",
+      //   onEnter: () => console.log("enter"),
+      //   onLeave: () => console.log("leave"),
+      //   onEnterBack: () => console.log("enterBack"),
+      //   onLeaveBack: () => console.log("leaveBack"),
+      //   onToggle: self => console.log("active", self.isActive),
+      //   id: "4"
+      // });
 
       // only show the relevant section's markers at any given time
       // gsap.set(
